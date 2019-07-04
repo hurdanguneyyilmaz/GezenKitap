a2e3f494fdec51aba0a9f78ec382e28fa83d77d0
Güney <48156435+hurdanguneyyilmaz@users.noreply.github.com>
2019-07-04 17:17:29 +0300

    son

---
+++ GeZeNKiTaP/GezenKitap/GezenKitap.DATA/EnumsInterface/Enums.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GezenKitap.DATA.EnumsInterface
+{
+    public enum OrderState
+    {
+        Iptal = -1,
+        Istek = 0,
+        Kargolandi = 1,
+        Tamamlandi = 2
+    }
+
+    //public enum Cinsiyet
+    //{
+    //    Kiz = 0,
+    //    Erkek = 1
+    //}
+
+    //public enum Gozluk
+    //{
+    //    Gozluksuz = 0,
+    //    Gozluklu = 1
+    //}
+
+    //public class Ogrenci
+    //{
+    //    public string Adi { get; set; }
+    //    public string Soyadi { get; set; }
+    //    public int NO { get; set; }
+    //    public Cinsiyet cinsiyet { get; set; }
+    //    public Gozluk gozluk { get; set; }
+    //}
+
+    //public class Sinif
+    //{
+    //    public List<Ogrenci> ogrenciler { get; set; }
+
+    //    public IEnumerable<Ogrenci> ErkekOgrenciler()//entityframework kullanmadığımız için IEnumerable kullandık.
+    //    {
+    //        return ogrenciler.Where(x => x.cinsiyet == Cinsiyet.Erkek);
+    //    }
+
+
+    //}
+}
---
+++ GeZeNKiTaP/GezenKitap/GezenKitap.UI/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using GezenKitap.DAL;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GezenKitap.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var userid = User.Identity.GetUserId();
+            ApplicationDbContext db = new ApplicationDbContext();//kullanıcı hem kendi kitabını göremesin hem silinmiş kitaplar gelmesin hem de satılmamış kitaplar(aktif olanlar) gelsin.
+            TempData["Kitaplar"] = db.Books.Where(x => x.UserID != userid && x.IsDelete == false && x.IsActive == true).OrderBy(r => Guid.NewGuid()).Take(8).ToList();
+            return View(); //'TempData["Kitaplar"] ve var model' ikiside aynı işi yapıyor.
+            //var model = db.Books.Where(x => x.UserID != userid && x.IsDelete == false && x.IsActive == true).OrderBy(r => Guid.NewGuid()).Take(8).ToList();
+
+            //return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
